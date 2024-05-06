30980a87d56f2ff853f8305b7cb47f622efa486d
Sunami Dasgupta <sunamidasgupta@gmail.com>
2024-05-05 17:28:25 -0700

    Tests for match() method

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -20,3 +20,37 @@ TEST(GuesserTest, smoke_test)
     ASSERT_EQ( 1+1, 2 );
 }
 */
+
+TEST(GuesserTest, empty_string_match){
+	Guesser myguess = Guesser("");
+	ASSERT_TRUE(myguess.match(""));
+}
+TEST(GuesserTest, single_space_string_not_match){
+	Guesser myguess = Guesser("");
+	ASSERT_FALSE(myguess.match(" "));
+}
+
+TEST(GuesserTest, perfect_alphabet_lowercase_character_match){
+	Guesser myguess = Guesser("acdfgr");
+	ASSERT_TRUE(myguess.match("acdfgr"));
+}
+TEST(GuesserTest, perfect_alphabet_mixedcase_character_match){
+	Guesser myguess = Guesser("acDfgR");
+	ASSERT_TRUE(myguess.match("acdfgR"));
+}
+TEST(GuesserTest, perfect_mixed_character_match){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	ASSERT_TRUE(myguess.match("ac#$%@#dfgr"));
+}
+TEST(GuesserTest, perfect_alphabet_lowercase_character_not_match){
+	Guesser myguess = Guesser("afgr456f");
+	ASSERT_TRUE(myguess.match("acdfgr"));
+}
+TEST(GuesserTest, perfect_alphabet_mixedcase_character_not_match){
+	Guesser myguess = Guesser("acDfgR");
+	ASSERT_TRUE(myguess.match("acdfgR2323d"));
+}
+TEST(GuesserTest, perfect_mixed_character_not_match){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	ASSERT_TRUE(myguess.match("ac#$%@#dfgR"));
+}
