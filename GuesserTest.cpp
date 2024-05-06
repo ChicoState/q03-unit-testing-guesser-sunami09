c9e32249c503e354b2a18724b64617cb30f2882a
Sunami Dasgupta <sunamidasgupta@gmail.com>
2024-05-05 18:38:24 -0700

    Tests for remaining() and distance method

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -36,7 +36,7 @@ TEST(GuesserTest, perfect_alphabet_lowercase_character_match){
 }
 TEST(GuesserTest, perfect_alphabet_mixedcase_character_match){
 	Guesser myguess = Guesser("acDfgR");
-	ASSERT_TRUE(myguess.match("acdfgR"));
+	ASSERT_TRUE(myguess.match("acDfgR"));
 }
 TEST(GuesserTest, perfect_mixed_character_match){
 	Guesser myguess = Guesser("ac#$%@#dfgr");
@@ -44,13 +44,137 @@ TEST(GuesserTest, perfect_mixed_character_match){
 }
 TEST(GuesserTest, perfect_alphabet_lowercase_character_not_match){
 	Guesser myguess = Guesser("afgr456f");
-	ASSERT_TRUE(myguess.match("acdfgr"));
+	ASSERT_FALSE(myguess.match("acdfgr"));
 }
 TEST(GuesserTest, perfect_alphabet_mixedcase_character_not_match){
 	Guesser myguess = Guesser("acDfgR");
-	ASSERT_TRUE(myguess.match("acdfgR2323d"));
+	ASSERT_FALSE(myguess.match("acdfgR2323d"));
 }
 TEST(GuesserTest, perfect_mixed_character_not_match){
 	Guesser myguess = Guesser("ac#$%@#dfgr");
-	ASSERT_TRUE(myguess.match("ac#$%@#dfgR"));
+	ASSERT_FALSE(myguess.match("ac#$%@#dfgR"));
+}
+TEST(GuesserTest, char_32_not_match){
+	Guesser myguess = Guesser("ac#$%@#dfgrsdfghjhgfdsdfghgfdsderftgbvcxsdfgbvcxsdfgbvcxdcfvgb");
+	ASSERT_FALSE(myguess.match("ac#$%@#dfgRsdcfvcxcvbvcscvbvcxz"));
+}
+TEST(GuesserTest, char_32_match){
+	Guesser myguess = Guesser("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+	ASSERT_TRUE(myguess.match("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+}
+
+
+TEST(GuesserTest, initial_guess_remaining){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	ASSERT_EQ(3, myguess.remaining());
+}
+
+TEST(GuesserTest, guessed_once_remaining){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	myguess.match("ac#$%@#df");
+	ASSERT_EQ(2, myguess.remaining());
+}
+TEST(GuesserTest, guessed_twice_remaining){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	myguess.match("ac#$%@#df");
+	myguess.match("ac#$%@#dfg");
+	ASSERT_EQ(1, myguess.remaining());
+}
+TEST(GuesserTest, guessed_thrice_remaining){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	myguess.match("ac#$%@#df");
+	myguess.match("ac#$%@#dfg");
+	myguess.match("ac#$%@#dfgrf");
+	ASSERT_EQ(0, myguess.remaining());
+}
+TEST(GuesserTest, guessed_once_locked_remaining){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	myguess.match("ac#$f");
+	ASSERT_EQ(0, myguess.remaining());
+}
+TEST(GuesserTest, guessed_twice_locked_remaining){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	myguess.match("ac#$%@#df");
+	myguess.match("ac#$f");
+	ASSERT_EQ(0, myguess.remaining());
+}
+TEST(GuesserTest, guessed_thrice_locked_remaining){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	myguess.match("ac#$%@#df");
+	myguess.match("ac#$%@#dfg");
+	myguess.match("ac#$f");
+	ASSERT_EQ(0, myguess.remaining());
+}
+
+TEST(GuesserTest, guessed_once_set_remaining_to_initial){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	myguess.match("ac#$%@#dfgr");
+	
+	ASSERT_EQ(3, myguess.remaining());
+}
+TEST(GuesserTest, guessed_twice_set_remaining_to_initial){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	myguess.match("ac#$%@#df");
+	myguess.match("ac#$%@#dfgr");
+	ASSERT_EQ(3, myguess.remaining());
+}
+TEST(GuesserTest, guessed_thrice_set_remaining_to_initial){
+	Guesser myguess = Guesser("ac#$%@#dfgr");
+	myguess.match("ac#$%@#df");
+	myguess.match("ac#$%@#dfg");
+	myguess.match("ac#$%@#dfgr");
+	ASSERT_EQ(3, myguess.remaining());
+}
+
+
+TEST(GuesserTest, distance_zero){
+	Guesser myguess = Guesser("guess");
+	myguess.match("guess");
+	ASSERT_EQ(0, myguess.get_distance());
+}
+
+TEST(GuesserTest, distance_one_space){
+	Guesser myguess = Guesser("guess");
+	myguess.match("guess ");
+	ASSERT_EQ(1, myguess.get_distance());
+}
+
+TEST(GuesserTest, distance_two_uppercase_space){
+	Guesser myguess = Guesser("guess");
+	myguess.match("guesS ");
+	ASSERT_EQ(2, myguess.get_distance());
+}
+
+TEST(GuesserTest, distance_secret_size_maximum){
+	Guesser myguess = Guesser("guess");
+	myguess.match("guessguessguessguessguess");
+	ASSERT_EQ(5, myguess.get_distance());
+}
+
+TEST(GuesserTest, distance_secret_size_maximum_reverse){
+	Guesser myguess = Guesser("guessguessguessguessguess");
+	myguess.match("guess");
+	ASSERT_EQ(20, myguess.get_distance());
+}
+TEST(GuesserTest, Distance_EqualLength) {
+    Guesser guesser("password");
+    guesser.match("passw0rd");
+    ASSERT_EQ(1, guesser.get_distance());  // 'o' and '0' are different.
+}
+TEST(GuesserTest, Distance_ShorterGuess) {
+    Guesser guesser("password123");
+    guesser.match("password");
+    ASSERT_EQ(3, guesser.get_distance());  // Missing "123"
+}
+
+TEST(GuesserTest, distance_locked){
+	Guesser myguess = Guesser("guess");
+	myguess.match("guessguessguessguessguess");
+	ASSERT_FALSE(myguess.match("guess"));
+}
+
+TEST(GuesserTest, distance_locked_zero){
+	Guesser myguess = Guesser("guess");
+	myguess.match("guessguessguessguessguess");
+	ASSERT_EQ(0, myguess.remaining());
 }
